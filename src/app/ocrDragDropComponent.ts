36def315b8101a64f5c1f8fcf3ee2cb60d9d867b
Juper123 <tp.glowacki@gmail.com>
2020-03-23 14:44:47 +0100

    Initial commit

---
+++ src/app/ocrDragDropComponent.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'cdk-drag-drop-overview-example',
+  templateUrl: 'cdk-drag-drop-overview-example.html',
+  styleUrls: ['cdk-drag-drop-overview-example.css'],
+})
+export class CdkDragDropOverviewExample {
+
+ todo = [
+    'one',
+    'two',
+    'three',
+    'four'
+  ];
+
+}
